7fc42a00b1ca2cef1080cf434743fb8cce0747ce
Bouchbout-sami <ks_bouchbout@esi.dz>
2023-03-17 02:18:41 +0100

    Initialized IWD project

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="text-7xl text-greenIWD font-SofiaBold">app</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      SofiaBold: ["SofiaBold"],
+      SofiaRegular: ["SofiaRegular"],
+    },
+    colors: {
+      yellowIWD: "#F9D207",
+      greenIWD: "#2ACF8A",
+      grayIWD: "#696969",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
